36f40adf8e6563845d7a674ede74d2ef03447f1c
rockthrone <adam@xstreamtrucking.com>
2021-03-09 12:56:10 -0600

    creative draft v1

--- pretty_print.c
+++ pretty_print.c
@@ -1,13 +1,26 @@
 #include "dbg.h"
-
-
+#include <stdlib.h>
+#include <string.h>
+const size_t MAX_LINE = 1024;
 
 
 int main (int argc, char *argv[])
 {
-    // update
-    // another update
+    char *line = calloc(MAX_LINE, 1);  
+    FILE *file = fopen(argv[1], "r");
 
+    while (fgets(line, MAX_LINE, file)){
+        for (int i = 0; i < MAX_LINE; i++){
+            if (line[i] == '}' ||
+                line[i] == ','){
+                printf("%c\n", line[i]);
+            }
+            else {
+                printf("%c", line [i]);
+            }
+        } 
+    }
+ 
 
     return 0;
 }
